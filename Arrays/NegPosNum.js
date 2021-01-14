a3873b6280df3f9ecfb69dbf55fd52461ac37c13
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-14 10:55:21 +0200

    Initial Commit

---
+++ Arrays/NegPosNum.js
@@ -0,0 +1,11 @@
+function rearangeArr(arr){
+    newArr =[]
+    for (let num of arr) {
+        if (num <0){
+            newArr.unshift(num)
+        }else{
+            newArr.push(num)
+        }        
+    }
+    console.log(newArr.join('\n'));
+}
