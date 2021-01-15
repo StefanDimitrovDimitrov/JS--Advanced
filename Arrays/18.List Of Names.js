f8d4cc8c8b53686b72291fb399c4d49f0adfb343
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-15 14:41:02 +0200

    Initial Commit

---
+++ Arrays/18.List Of Names.js
@@ -0,0 +1,6 @@
+function listOfNames(arr){
+
+    arr.sort((a, b) => a.localeCompare(b)).forEach(x => console.log(`${arr.indexOf(x)+1}.${x}`) )
+}
+
+listOfNames(["John", "Bob", "Christina", "Ema"])
