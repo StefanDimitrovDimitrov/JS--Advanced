247830152811ba312102e50f1dfab1b7157f897a
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-15 17:26:01 +0200

    Initial Commit

--- Arrays/18.List Of Names.js
+++ Arrays/18.List Of Names.js
@@ -1,5 +1,4 @@
 function listOfNames(arr){
-
     arr.sort((a, b) => a.localeCompare(b)).forEach(x => console.log(`${arr.indexOf(x)+1}.${x}`) )
 }
 
---
+++ Arrays/19.Sorting Numbers.js
@@ -0,0 +1,16 @@
+function sorting(arr){
+    newArr = []
+
+    arr.sort((a,b) => a-b)
+        
+    while (arr.length > 0){
+        newArr.push(arr.shift())
+        newArr.push(arr.pop())
+    }
+
+
+    return newArr
+
+}
+
+console.log(sorting([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]))
