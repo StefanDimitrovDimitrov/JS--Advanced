9d3f3ebfd448802738bd176c9dd970c73dcc04c5
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-18 11:33:00 +0200

    Initial Commit

--- Arrays/21.Magic Matrices.js
+++ Arrays/21.Magic Matrices.js
@@ -1,13 +1,15 @@
 function matrix(arr){
 
     let resultRow = arr.map((el)=> el.reduce((a,b)=>a+b),0);
+    console.log(resultRow)
     let resultColm = arr.reduce((a, b)=> a.map((x, i)=> x + b[i]))
+    console.log(resultColm);
     return resultRow.concat(resultColm).every((el, i, arr) => el === arr[0]);
 
 }
 
 console.log(matrix(
-    [[1, 0, 0],
-    [0, 0, 1],
-    [0, 1, 0]]
+    [[100, 100, 100],
+    [50, 50, 50],
+    [1, 2, 3]]
    ))
