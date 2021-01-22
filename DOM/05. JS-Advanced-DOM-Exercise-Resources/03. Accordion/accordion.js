21825c9157650810b4ac1958197a70949bbbfc16
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-22 14:47:49 +0200

    Initial Commit

--- DOM/05. JS-Advanced-DOM-Exercise-Resources/03. Accordion/accordion.js
+++ DOM/05. JS-Advanced-DOM-Exercise-Resources/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.getElementsByClassName('button')[0];
+    let info = document.getElementById('extra');
+    if(btn.textContent == "More"){
+        info.style.display = "block";
+        btn.innerText = "Less"; //btn.textContent is the same
+    }else{
+        btn.innerHTML = "More";
+        info.style.display = 'none';
+    }
 }
