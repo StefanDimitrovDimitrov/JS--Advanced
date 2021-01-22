7d0359779da649502895dfe0b2d835bd758551cd
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-22 23:19:49 +0200

    Initial Commit

--- DOM/05. JS-Advanced-DOM-Exercise-Resources/02.5 Pascal or Camel Case/solution.js
+++ DOM/05. JS-Advanced-DOM-Exercise-Resources/02.5 Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  let text = getElementById("text").value
+  let convention = getElementById("naming-convention").value
+  
+  let btn = document.getElementsByTagName("button")[0];
+  btn.addEventListener("click", result)
+  text = text.split(" ");
+
+  function result(){
+    if (convention = "Camel Case"){
+      result = []
+      firstWord = text.shift().toLowerCase
+      result.push(firstword)
+      for (const word of text) {
+        word[0].toUpperCase
+        result.push(word)
+      }
+    }
+    // }else if(convention = "Pascal Case"){
+    // //'''
+    // }else{
+    //   //...
+    // }
+  }
 }
