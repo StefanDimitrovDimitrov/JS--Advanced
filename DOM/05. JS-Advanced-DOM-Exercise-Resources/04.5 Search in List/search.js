79e8335afed0d35ab17139a4fe5a6a4d94c36c28
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-23 22:17:20 +0200

    Initial commit

--- DOM/05. JS-Advanced-DOM-Exercise-Resources/04.5 Search in List/search.js
+++ DOM/05. JS-Advanced-DOM-Exercise-Resources/04.5 Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   // take the input 
+   // loop over towns 
+   // creat Array with the matched towns 
+
+   let input = document.getElementById("searchText").value
+   let counter = 0;
+   let list = document.getElementsByTagName("li")
+   for (let town of list) {
+      if(town.innerHTML.includes(input)){
+         counter++
+         town.style.fontWeight  = "bold"
+         town.style.textDecoration  = "underline" 
+      }else{
+         town.style.fontWeight  = "normal"
+         town.style.textDecoration  = "none"  
+      }
+      document.getElementById("result").innerHTML = `${counter} matches found`
+   }
 }
--- DOM/05. JS-Advanced-DOM-Exercise-Resources/08.5 Generate Report/report.js
+++ DOM/05. JS-Advanced-DOM-Exercise-Resources/08.5 Generate Report/report.js
@@ -1,3 +1,45 @@
 function generateReport(colNames) {
-    //TODO
+    let checkBoxes = document.getElementsByTagName("input")
+    let data = document.getElementsByTagName("td")
+    let rows = document.getElementsByTagName("tr")
+
+    let checked = []
+    let resultObject = {}
+    
+    let resultList = []
+
+    for (const index in checkBoxes) {
+        if (checkBoxes[index].checked){
+            checked.push(Number(index))
+        }
+    }
+
+    for (const check of checked) {
+        let nameField = checkBoxes[check].name
+        resultObject[nameField] = ''
+    }
+    
+    // let fieldContent = data[g]
+
+    for (let g = 1; g < rows.length; g++) {
+        let row = rows[g]
+        let rowsValue = rows[g].children
+        let resultObject2 = {}
+        for (let z = 0; z < row.cells.length; z++) {
+            key = checkBoxes[z].name
+            
+            if (resultObject.hasOwnProperty(key)){
+                resultObject2[key] = rowsValue[z].innerText
+               
+            }
+        }
+        resultList.push(resultObject2)
+        console.log(resultList);   
+    }
+
+    result = JSON.stringify(resultList)
+
+    document.getElementById("output").innerText = result
 }
+
+
