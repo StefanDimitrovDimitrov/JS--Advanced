c10f9ec0503212c00bd325b625acb5f5d09b0fa1
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-25 13:53:29 +0200

    Initial Commit

--- DOM/05. JS-Advanced-DOM-Exercise-Resources/06.5 Format the Text/solution.js
+++ DOM/05. JS-Advanced-DOM-Exercise-Resources/06.5 Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  input = document.getElementById("input").value.split(".");
+  let result = '';
+  input.pop();
+  let sentances = []
+  for (let i = 0; i < input.length; i++) {
+    text = input[i]
+    if (sentances.length < 3){
+      if(text != 0){
+        sentances.push(text)
+      }
+    }else{
+      result += `<p>${sentances.join(".")}.</p>`
+      sentances = []
+      sentances.push(text)
+    }
+  }
+  result += `<p>${sentances.join(".")}.</p>`
+
+  document.getElementById("output").innerHTML = result
 }
