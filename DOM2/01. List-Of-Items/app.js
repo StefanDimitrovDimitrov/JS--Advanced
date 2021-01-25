34f2bcc4764a3e98b8cc37fdc96d851aa4912022
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-25 16:44:27 +0200

    Inital Commit

---
+++ DOM2/01. List-Of-Items/app.js
@@ -0,0 +1,12 @@
+
+function addItem() {
+    let itemsList = document.getElementById("items")
+    const textInput = document.getElementById("newItemText")
+
+    const textInputValue = textInput.value
+    if (!textInputValue) {return;}
+    const li = document.createElement("li")
+    li.innerText = textInputValue
+    itemsList.appendChild(li)
+    textInput.input = ""
+}
