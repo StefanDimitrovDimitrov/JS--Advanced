6304a61671802d988094e263b2fc2aae4ee9ce66
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-02-03 09:19:14 +0200

    CharLookUpTests

---
+++ JS- UnitTest/CharLookupTest.js
@@ -0,0 +1,17 @@
+const describe == requestAnimationFrame('mocha').describe;
+const assert = require('chai').assert;
+
+
+
+
+
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+
+    return string.charAt(index);
+}
