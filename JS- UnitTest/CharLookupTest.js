95cbc12601e925643779e4b5a0cf6fb3936ad8c3
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-02-03 09:38:27 +0200

    CharLookUpTest

--- JS- UnitTest/CharLookupTest.js
+++ JS- UnitTest/CharLookupTest.js
@@ -1,9 +1,5 @@
-const describe == requestAnimationFrame('mocha').describe;
+const describe = requestAnimationFrame('mocha').describe;
 const assert = require('chai').assert;
-
-
-
-
 
 function lookupChar(string, index) {
     if (typeof(string) !== 'string' || !Number.isInteger(index)) {
@@ -15,3 +11,26 @@ function lookupChar(string, index) {
 
     return string.charAt(index);
 }
+
+
+
+describe('charLookup', () => {
+    it('', () => {  
+        assert.isUndefined(lookupChar(1, 1));
+        assert.isUndefined(lookupChar('abc', 1.2));
+        assert.isUndefined(lookupChar('abc', 'a'));
+    });
+    
+    it('', () => {
+        assert.equal(lookupChar('abv', -1), "Incorrect index");
+        assert.equal(lookupChar('abv', 4), "Incorrect index");
+
+    });
+
+    it('', () => {
+        assert.equal(lookupChar('abv', 0), 'a');
+        assert.equal(lookupChar('abv', 2), 'v');
+
+    });
+
+})
