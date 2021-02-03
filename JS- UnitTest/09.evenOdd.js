4a8ac438ef9ca7f9c9fe0dd57dae7f189e8b89c6
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-02-03 09:38:03 +0200

    EvenOdd - mocha +chai

--- JS- UnitTest/09.evenOdd.js
+++ JS- UnitTest/09.evenOdd.js
@@ -3,12 +3,12 @@ const assert = require('chai').assert;
 
 describe ('check isOffOrEven', () => {
     it('Type is string', () => {
-        assert.equal(isODDOrEven(1), 'Message a==a')
+        assert.isUndefined(isOddOrEven(1), 'Message a==a')
     })
     it('Is even', () => {
         assert.equal(isOddOrEven('aa'),'even', 'Message a==a')
     })
-    it('Is off', () => {
+    it('Is odd', () => {
         assert.equal(isOddOrEven('a'),'odd', 'Message a==a')
     })
 })
