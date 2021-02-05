fc6c7aafc1fffbdcbf178bbf8d008ec6ae84875b
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-02-05 10:32:13 +0200

    Circle Class

---
+++ Objects/Circle.js
@@ -0,0 +1,33 @@
+class Circle {
+    constructor(r) {
+        this.r = r
+
+    }
+
+    get diameter() {
+        return this.r * 2;
+
+    }
+
+    set diameter(value) {
+        if (typeof value != 'number') {
+            throw new TypeError('Diameter must be a number')
+        }
+        this.r = value / 2;
+    }
+
+    get area(){
+        return this.r ** 2 * Math.PI;
+    }
+
+    get radius(){
+        return this.r
+    }
+
+
+}
+
+let c = new Circle(2);
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
