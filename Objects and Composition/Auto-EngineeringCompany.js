55c4d1866759f72e63e217e6b0ab007634f31ef7
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-02-08 12:41:26 +0200

    Update Auto-EngineeringCompany.js

--- Objects and Composition/Auto-EngineeringCompany.js
+++ Objects and Composition/Auto-EngineeringCompany.js
@@ -1,20 +1,53 @@
 function solve(arr) {
-    let result = arr
-        .map((line) => line.split(' | '))
-        .reduce((storage,cars) => {
-            let[brand,model,quantity] = cars;
-            if(!storage[brand]) {
-                storage[brand] = {};
-            }
-            if (!storage[brand][model]){
-                storage[brand][model] = 0;
-            }
+    let splitted = []
 
-            storage[brand][model] += Number(quantity);
-            return storage;
-        },{})
+    for(let i = 0; i< arr.length; i++) {
+        splitted.push(arr[i].split(' | '))
+    }
 
-        return result
+    let storage = new Map();
+
+    for(let i = 0; i< splitted.length; i++) {
+        let cars = splitted[i];
+
+        let[brand,model,quantity] = cars;
+        if(!storage.has(brand)){
+            storage.set(brand, new Map());
+        }
+    
+        if(!storage.get(brand).has(model)){
+            storage.get(brand).set(model, 0);
+        }
+        let value = Number(storage.get(brand).get(model))
+        storage.get(brand).set(model, value + Number(quantity));
+    }
+
+
+        let resultString = [];
+        for ([brand,model] of storage.entries()) {
+            let result = `${brand}\n`;
+            for(let[name,quantity]of model.entries())
+                result += `###${name}->${quantity}\n`
+            resultString.push(result.trim());
+        }
+        return resultString.join('\n');
+
+        // let result = arr
+        // .map((line) => line.split(' | '))
+        // .reduce((storage,cars) => {
+        //     let[brand,model,quantity] = cars;
+        //     if(!storage[brand]) {
+        //         storage[brand] = {};
+        //     }
+        //     if (!storage[brand][model]){
+        //         storage[brand][model] = 0;
+        //     }
+
+        //     storage[brand][model] += Number(quantity);
+        //     return storage;
+        // },{})
+
+        // return result
 }
 
 console.log(solve(['Audi | Q7 | 1000',
